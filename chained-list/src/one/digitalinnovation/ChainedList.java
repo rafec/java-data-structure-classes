4da388e293fe7bdb88fb53fbe36ff2a284c5742c
rafec <rafael.brizuena@gmail.com>
2023-03-24 20:29:48 -0400

    add() implementation

--- chained-list/src/one/digitalinnovation/ChainedList.java
+++ chained-list/src/one/digitalinnovation/ChainedList.java
@@ -1,10 +1,28 @@
 package one.digitalinnovation;
 
+import java.util.Iterator;
+
 public class ChainedList<T> {
 	Node<T> entryReference;
 	
 	public ChainedList() {
 		this.entryReference = null;
+	}
+	
+	public void add(T content) {
+		Node<T> newNode = new Node<>(content);
+		
+		if (this.isEmpty()) {
+			entryReference = newNode;
+			return;
+		}
+		
+		Node<T> auxNode = entryReference;
+		for(int i = 0; i < this.size()-1; i++) {
+			auxNode = auxNode.getNextNode();
+		}
+		
+		auxNode.setNextNode(newNode);
 	}
 	
 	public int size() {
